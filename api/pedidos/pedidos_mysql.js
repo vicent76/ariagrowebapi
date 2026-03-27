124b53aff425f2efa903f9ff5e76b386f831ded6
vicent <vicent@myariadna.com>
2026-03-27 12:02:04 +0100

    ajiuste sql

--- api/pedidos/pedidos_mysql.js
+++ api/pedidos/pedidos_mysql.js
@@ -67,7 +67,7 @@ const productos_mysql = {
                 LEFT JOIN confpale AS pa ON pa.codpalet = pv.codpalet
                 LEFT JOIN calibres AS ca ON ca.codvarie = pc.codvarie AND ca.codcalib = pc.codcalib 
                 WHERE p.fechacar = '${fecha}'
-               /*  AND al.numalbar IS NULL */
+                AND al.numalbar IS NULL
 
                 GROUP BY p.numpedid, pv.numlinea
                 ORDER BY p.fechaped, p.numpedid, c.nomclien;
